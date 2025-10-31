9879330c3568412e5d8020c0b675cf6d1c3368d7
sharkmu <csonka.samuel@gmail.com>
2025-10-31 22:33:56 +0100

    complete logic
    I hope it works properly :D

--- calculator.nim
+++ calculator.nim
@@ -1,1 +1,36 @@
-echo "This is a calculator"
+import strutils
+
+echo "--- This is a calculator ---\n"
+echo "Please enter the first number:"
+let num1 = readLine(stdin)
+echo "Please enter the second number:"
+let num2 = readLine(stdin)
+echo "Please choose an operation:"
+let operation = readLine(stdin)
+
+func inputToInt(a: string, b: string): (int, int) =
+    return (parseInt(a), parseInt(b))
+
+func addition(a: string, b: string): int =
+    let nums = inputToInt(a, b)
+    return nums[0]+nums[1]
+
+func subtraction(a: string, b: string): int =
+    let nums = inputToInt(a, b)
+    return nums[0]-nums[1]
+
+func multiplication(a: string, b: string): int =
+    let nums = inputToInt(a, b)
+    return nums[0]*nums[1]
+
+func division(a: string, b: string): int =
+    let nums = inputToInt(a, b)
+    return toInt(nums[0]/nums[1])
+
+case operation
+of "+": echo $addition(num1, num2)
+of "-": echo $subtraction(num1, num2)
+of "*": echo $multiplication(num1, num2)
+of "/": echo $division(num1, num2)
+of ":": echo $division(num1, num2)
+else: echo "No such operation"
